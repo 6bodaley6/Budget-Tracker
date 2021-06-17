e409ea6a5329fb63de62fcd349c1e16aa0b8d4ad
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2021-06-17 12:19:33 -0600

    passes all lighthouse audits ready to push to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,30 @@
-const APP_PREFIX = 'Budget-Tracker';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "/",
-    "/index.html",
-    "db.js",
-    "favicon.ico",
-    "index.js",
-    "styles.css",
-    ".manifest.webmanifest",
+    "./index.html",
+    "./db.js",
+    "./favicon.ico",
+    "./index.js",
+    "./styles.css",
+    "./manifest.json",
     "service-worker.js",
-    "maskable_icon.png",
-    "/icons/maskable_icon_x512.png",
-    "/icons/maskable_icon_x384.png",
-    "/icons/maskable_icon_x192.png",
-    "/icons/maskable_icon_x128.png",
-    "/icons/maskable_icon_x96.png",
-    "/icons/maskable_icon_x72.png",
-    "/icons/maskable_icon_x48.png",
+    "./maskable_icon.png",
+    "./icons/maskable_icon_x512.png",
+    "./icons/maskable_icon_x384.png",
+    "./icons/maskable_icon_x192.png",
+    "./icons/maskable_icon_x128.png",
+    "./icons/maskable_icon_x96.png",
+    "./icons/maskable_icon_x72.png",
+    "./icons/maskable_icon_x48.png",
 ];
-const CACHE_NAME = 'static-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1'
+const APP_PREFIX = 'Budget_Tracker';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+// // const CACHE_NAME = 'static-cache-v1';
+// // const DATA_CACHE_NAME = 'data-cache-v1'
 
 //Installation
-self.addEventListener("install", function (evt) {
-    evt.waitUntil(
+self.addEventListener("install", function (e) {
+    e.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
                 console.log("your files were pre-cached successfully");
@@ -46,8 +46,8 @@ self.addEventListener("install", function (evt) {
 });
 
 //activate
-self.addEventListener("activate", function (evt) {
-    evt.waitUntil(
+self.addEventListener("activate", function (e) {
+    e.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(
                 keyList.map((key) => {
@@ -63,33 +63,33 @@ self.addEventListener("activate", function (evt) {
 });
 
 //fetch
-self.addEventListener("fetch", function (evt) {
-    if (evt.request.url.includes("/api/")) {
-        evt.respondWith(
+self.addEventListener("fetch", function (e) {
+    if (e.request.url.includes("/api/")) {
+        e.respondWith(
             caches
                 .open(DATA_CACHE_NAME)
                 .then((cache) => {
-                    return fetch(evt.request)
+                    return fetch(e.request)
                         .then((response) => {
                             //if response good clone it and store it in cache
                             if (response.status === 200) {
-                                cache.put(evt.request.url, response.clone());
+                                cache.put(e.request.url, response.clone());
                             }
                             return response;
                         })
                         .catch((err) => {
                             //network request failed try to get it from the cache
-                            return cache.match(evt.request);
+                            return cache.match(e.request);
                         });
                 })
                 .catch((err) => console.log(err))
         );
         return;
     }
-    evt.respondWith(
+    e.respondWith(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.match(evt.request).then((response) => {
-                return response || fetch(evt.request);
+            return cache.match(e.request).then((response) => {
+                return response || fetch(e.request);
             });
         })
     );
